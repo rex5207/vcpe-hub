4e677a264802e55d3860ee07781be782aa74759a
vicky <lch82327@gmail.com>
2015-12-22 17:14:40 +0800

    shorten code

--- firewall/simple_firewall.py
+++ firewall/simple_firewall.py
@@ -64,11 +64,11 @@ class SimpleFirewall(app_manager.RyuApp):
             # if port == 0, means block all protocol
             if port >= 0:
                 if trans_proto == inet.IPPROTO_TCP:
-                    match_dict.update({'ip_proto': trans_proto})
-                    match_dict.update({'tcp_dst': port})
+                    match_dict.update({'ip_proto': trans_proto,
+                                       'tcp_dst': port})
                 else:  # udp
-                    match_dict.update({'ip_proto': trans_proto})
-                    match_dict.update({'udp_dst': port})
+                    match_dict.update({'ip_proto': trans_proto,
+                                       'udp_dst': port})
 
             if len(src_ip) > 0:  # not ''
                 match_dict.update({'ipv4_src': src_ip})
@@ -104,65 +104,53 @@ class SimpleFirewallController(ControllerBase):
         protocol = str(json_data.get('protocol'))
 
         rule = {'state': state}
-        rule.update({'src_ip': src_ip})
-        rule.update({'dst_ip': dst_ip})
+        rule.update({'src_ip': src_ip, 'dst_ip': dst_ip})
         if protocol == 'HTTP':
             # HTTP -> TCP 80
-            rule.update({'trans_proto': inet.IPPROTO_TCP})
-            rule.update({'port': 80})
+            rule.update({'trans_proto': inet.IPPROTO_TCP, 'port': 80})
             simple_firewall.add_block_rule(**rule)
         elif protocol == 'FTP':
             # FTP -> TCP 20
-            rule.update({'trans_proto': inet.IPPROTO_TCP})
-            rule.update({'port': 20})
+            rule.update({'trans_proto': inet.IPPROTO_TCP, 'port': 20})
             simple_firewall.add_block_rule(**rule)
             # FTP -> TCP 21
             rule.update({'port': 21})
             simple_firewall.add_block_rule(**rule)
         elif protocol == 'SSH':
             # SSH -> TCP 22
-            rule.update({'trans_proto': inet.IPPROTO_TCP})
-            rule.update({'port': 22})
+            rule.update({'trans_proto': inet.IPPROTO_TCP, 'port': 22})
             simple_firewall.add_block_rule(**rule)
         elif protocol == 'TELNET':
             # TELNET -> TCP 23
-            rule.update({'trans_proto': inet.IPPROTO_TCP})
-            rule.update({'port': 23})
+            rule.update({'trans_proto': inet.IPPROTO_TCP, 'port': 23})
             simple_firewall.add_block_rule(**rule)
         elif protocol == 'HTTPS':
             # HTTPS -> TCP 443
-            rule.update({'trans_proto': inet.IPPROTO_TCP})
-            rule.update({'port': 443})
+            rule.update({'trans_proto': inet.IPPROTO_TCP, 'port': 443})
             simple_firewall.add_block_rule(**rule)
         elif protocol == 'SMTP':
             # SMTP -> TCP 25
-            rule.update({'trans_proto': inet.IPPROTO_TCP})
-            rule.update({'port': 25})
+            rule.update({'trans_proto': inet.IPPROTO_TCP, 'port': 25})
             simple_firewall.add_block_rule(**rule)
         elif protocol == 'POP3':
             # POP3 -> TCP 110
-            rule.update({'trans_proto': inet.IPPROTO_TCP})
-            rule.update({'port': 110})
+            rule.update({'trans_proto': inet.IPPROTO_TCP, 'port': 110})
             simple_firewall.add_block_rule(**rule)
         elif protocol == 'NTP':
             # NTP -> TCP 123
-            rule.update({'port': 123})
-            rule.update({'trans_proto': inet.IPPROTO_TCP})
+            rule.update({'trans_proto': inet.IPPROTO_TCP, 'port': 123})
             simple_firewall.add_block_rule(**rule)
             # NTP -> UDP 123
             rule.update({'trans_proto': inet.IPPROTO_UDP})
             simple_firewall.add_block_rule(**rule)
         elif protocol == 'IMAP':
             # IMAP -> UDP 143
-            rule.update({'trans_proto': inet.IPPROTO_TCP})
-            rule.update({'port': 143})
+            rule.update({'trans_proto': inet.IPPROTO_TCP, 'port': 143})
             simple_firewall.add_block_rule(**rule)
         elif protocol == '':
             # all protocol
-            rule.update({'trans_proto': 0})
-            rule.update({'port': -1})
+            rule.update({'trans_proto': 0, 'port': -1})
             simple_firewall.add_block_rule(**rule)
-
         dic = {}
         body = json.dumps(dic)
         return Response(content_type='application/json', body=body)
