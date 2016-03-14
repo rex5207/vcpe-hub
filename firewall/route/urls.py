172e34977fd2a03edb4f39afa2cec382c219aa39
vicky <lch82327@gmail.com>
2016-03-14 11:28:40 +0800

    add parental control route

--- firewall/route/urls.py
+++ firewall/route/urls.py
@@ -1,4 +1,8 @@
-# set nat config
+# set firewall config
 url_set_acl_knownport = '/api/firewall/acl/knownport'
 url_set_acl_customport = '/api/firewall/acl/customport'
 url_get_acl = '/api/firewall/acl'
+
+# set parental control
+url_get_prnt_ctrl = "/api/firewall/prnt_ctl"
+url_set_prnt_ctrl = "/api/firewall/prnt_ctl"
