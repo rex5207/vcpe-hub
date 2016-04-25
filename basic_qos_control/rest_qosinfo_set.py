f9a9780b1c63c2d32459b1fb5a1fe3a074ecec37
Katy Wu <katy12032001@gmail.com>
2016-04-25 15:39:09 +0800

    l4 rate control

--- basic_qos_control/rest_qosinfo_set.py
+++ basic_qos_control/rest_qosinfo_set.py
@@ -60,11 +60,23 @@ class QosSetup(app_manager.RyuApp):
 
     def set_ratelimite_for_mac(self, mac, meter_id, group_id, state):
         """Set rate control for applications."""
-        if setup.ratelimite_setup_for_specialcase.get(group_id) is not None:
+        if setup.ratelimite_setup_for_specialcase_member.get(group_id) is not None:
             memberset = setup.ratelimite_setup_for_specialcase_member.get(group_id)
             memberset.update({mac: {'state': state, 'meter_id': int(meter_id)}})
         else:
             setup.ratelimite_setup_for_specialcase_member.update({group_id: {mac: {'state': state, 'meter_id': int(meter_id)}}})
+
+        ev = Qos_UpdateEvent('Update qos for flow')
+        self.send_event_to_observers(ev)
+        data_collection.meter_list.update({"drop": "-1"})
+
+    def set_ratelimite_for_l4(self, port, meter_id, group_id, state):
+        """Set rate control for applications."""
+        if setup.ratelimite_setup_for_specialcase_port.get(group_id) is not None:
+            memberset = setup.ratelimite_setup_for_specialcase_port.get(group_id)
+            memberset.update({port: {'state': state, 'meter_id': int(meter_id)}})
+        else:
+            setup.ratelimite_setup_for_specialcase_port.update({group_id: {port: {'state': state, 'meter_id': int(meter_id)}}})
 
         ev = Qos_UpdateEvent('Update qos for flow')
         self.send_event_to_observers(ev)
@@ -195,6 +207,17 @@ class QosSetupRest(ControllerBase):
         state = str(json_link.get('state'))
 
         self.get_qos_info.set_ratelimite_for_mac(mac, meter_id, group_id, state)
+
+    @route('rate_for_port', urls.url_flow_port, methods=['PUT'])
+    def set_flow_for_ratelimite_for_member(self, req, **kwargs):
+        port = str(kwargs['port'])
+        content = req.body
+        json_link = json.loads(content)
+        meter_id = str(json_link.get('meter_id'))
+        group_id = str(json_link.get('group_id'))
+        state = str(json_link.get('state'))
+
+        self.get_qos_info.set_ratelimite_for_l4(port, meter_id, group_id, state)
 
     @route('meter_data', urls.url_meter_set, methods=['PUT'])
     def set_meter_data_(self, req, **kwargs):
