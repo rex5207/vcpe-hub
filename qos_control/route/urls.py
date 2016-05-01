c17b933a7032742e51278093573d1d33fe0e31f2
Katy Wu <katy12032001@gmail.com>
2016-05-01 21:19:54 +0800

    qos policy

--- qos_control/route/urls.py
+++ qos_control/route/urls.py
@@ -8,8 +8,9 @@ url_meter_set = '/api/set_meter_info/{meterid}'
 url_capacity_set = '/api/set_capacity/{capacity}'
 
 '''rest qos set'''
-url_qos_app_list_get = '/api/get_app_list'
+url_qos_app_list_get = '/api/get_app_list/{groupid}'
 url_qos_meter_list_get = '/api/get_meter_list'
+url_qos_policy_get = '/api/get_qos_policy/{groupid}'
 
 url_member_list_for_app = '/api/get_member_for_app/{app}'
 
--- qos_control/setting/variable/constant.py
+++ qos_control/setting/variable/constant.py
@@ -3,7 +3,7 @@ import socket
 
 FlowClassification_IP = "192.168.0.174"
 Detect_switch_DPID_check = 0
-Detect_switch_DPID = 3
+Detect_switch_DPID = 1
 Gateway_IP = "192.168.0.1"
 Gateway_Mac = None
 Controller_IP = socket.gethostbyname(socket.gethostname())
