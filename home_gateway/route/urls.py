dbbe33b9a2143184464520bc35d708eb2b4a3ff2
vicky <lch82327@gmail.com>
2016-05-24 16:16:18 +0800

    rearrange api url

--- home_gateway/route/urls.py
+++ home_gateway/route/urls.py
@@ -1,10 +1,12 @@
-# set firewall config
+# firewall config
 url_set_acl_knownport = '/api/firewall/acl/knownport'
 url_set_acl_customport = '/api/firewall/acl/customport'
 url_get_acl = '/api/firewall/acl'
 
-# set nat config
+# nat config
+url_nat_config_init = '/api/nat/config/init'
+url_nat_config_save = '/api/nat/config/save'
+url_nat_config_get = '/api/nat/config'
 
-url_nat_config_init = '/api/nat/config_init'
-url_nat_config_save = '/api/nat/config_save'
-url_nat_config_get = '/api/nat/config_get'
+# dhcp config
+url_dhcp_config_get = '/api/dhcp/config'
